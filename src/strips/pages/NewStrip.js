bfc932564e581b592e8b4fecdce9a46e2336cdde
John Harper <john@harper.link>
2021-04-08 17:54:21 -0500

    basic routing

---
+++ src/strips/pages/NewStrip.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewStrip = () => {
+    return <h2>New Strip!</h2>
+};
+
+export default NewStrip;
---
+++ src/strips/pages/Strips.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Strips = () => {
+    return <h1>Strips!</h1>;
+};
+
+export default Strips;
