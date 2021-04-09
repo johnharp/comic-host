1d5bded85f75ba9d65e0e6bff7ce0ccb342bb5a3
John Harper <john@harper.link>
2021-04-08 21:48:25 -0500

    tweak chapter item

--- src/chapter/pages/Chapters.js
+++ src/chapter/pages/Chapters.js
@@ -5,19 +5,19 @@ const Chapters = () => {
     const CHAPTERS = [
         {
             id: "1",
-            name: "Book 1",
+            name: "Chapter 1",
             title: "Agatha Heterodyne and the Beetleburg Clank",
             image: "https://www.girlgeniusonline.com/books/1.jpg"
         },
         {
             id: "2",
-            name: "Book 2",
+            name: "Chapter 2",
             title: "Agatha Heterodyne and the Airship City",
             image: "https://www.girlgeniusonline.com/books/2.jpg"
         },
         {
             id: "3",
-            name: "Book 3",
+            name: "Chapter 3",
             title: "Agatha Heterodyne and the Monster Engine",
             image: "https://www.girlgeniusonline.com/books/3.jpg"
         }
