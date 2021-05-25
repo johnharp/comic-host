f41b232293b73d4a408a1552ecd6752fc292a471
John Harper <john@harper.link>
2021-05-25 18:55:03 -0500

    load chapters from DB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 require("dotenv").config();
 
@@ -15,6 +15,16 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
 
 app.use("/api/users", usersRoutes);
 app.use("/api/chapters", chaptersRoutes);
@@ -34,11 +44,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(process.env.MONGO_DB_URL)
-    .then(() => {
-        app.listen(5000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- frontend/src/chapter/pages/Chapters.js
+++ frontend/src/chapter/pages/Chapters.js
@@ -1,29 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import ChapterList from "../components/ChapterList";
 
-const Chapters = () => {
-    const CHAPTERS = [
-        {
-            id: "1",
-            name: "Chapter 1",
-            title: "Agatha Heterodyne and the Beetleburg Clank",
-            image: "https://www.girlgeniusonline.com/books/1.jpg"
-        },
-        {
-            id: "2",
-            name: "Chapter 2",
-            title: "Agatha Heterodyne and the Airship City",
-            image: "https://www.girlgeniusonline.com/books/2.jpg"
-        },
-        {
-            id: "3",
-            name: "Chapter 3",
-            title: "Agatha Heterodyne and the Monster Engine",
-            image: "https://www.girlgeniusonline.com/books/3.jpg"
-        }
-    ];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-    return <ChapterList items={CHAPTERS} />;
+const Chapters = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedChapters, setLoadedChapters] = useState();
+
+  const CHAPTERS = [
+    {
+      id: "1",
+      name: "Chapter 1",
+      title: "Agatha Heterodyne and the Beetleburg Clank",
+      image: "https://www.girlgeniusonline.com/books/1.jpg",
+    },
+    {
+      id: "2",
+      name: "Chapter 2",
+      title: "Agatha Heterodyne and the Airship City",
+      image: "https://www.girlgeniusonline.com/books/2.jpg",
+    },
+    {
+      id: "3",
+      name: "Chapter 3",
+      title: "Agatha Heterodyne and the Monster Engine",
+      image: "https://www.girlgeniusonline.com/books/3.jpg",
+    },
+  ];
+
+  // run only once
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/chapters");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedChapters(responseData.chapters);
+      } catch (err) {
+        setError(err.message);
+      }
+
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedChapters && <ChapterList items={loadedChapters} />}
+    </React.Fragment>
+  );
 };
 
 export default Chapters;
